a5ee0d4b827a3146bbe58d124e4df25ff36af561
Coding-Ghostman <atmanmishra1122@gmail.com>
2023-12-16 20:47:56 +0530

    Adding Models

---
+++ Model.py
@@ -0,0 +1,70 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras import layers, models
+from sklearn.model_selection import train_test_split
+import pickle
+
+# Set the seed for reproducibility
+seed = 42
+np.random.seed(seed)
+tf.random.set_seed(seed)
+
+# Define data directories
+data_dir = 'data'
+train_dir = data_dir
+
+# Define parameters
+batch_size = 32
+img_size = (224, 224)
+epochs = 10
+
+# Data augmentation and preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2
+)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode='binary',
+    subset='training'
+)
+
+validation_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode='binary',
+    subset='validation'
+)
+
+# Define DenseNet model
+base_model = DenseNet121(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+model = models.Sequential()
+model.add(base_model)
+model.add(layers.GlobalAveragePooling2D())
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(
+    train_generator,
+    epochs=epochs,
+    validation_data=validation_generator
+)
+
+# Save the model using pickle
+with open('covid_classification_model.pkl', 'wb') as model_file:
+    pickle.dump(model, model_file)
+clear
