d388c0a21f3c8ecc94432a953142924c4e8c65a0
Coding-Ghostman <atmanmishra1122@gmail.com>
2023-12-16 20:36:55 +0530

    Changing APP.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
-from cnnClassifier.utils.common import decodeImage
-from cnnClassifier.pipeline.predict import PredictionPipeline
 
 
 os.putenv('LANG', 'en_US.UTF-8')
@@ -10,13 +8,6 @@ os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
-
-
-class ClientApp:
-    def __init__(self):
-        self.filename = "inputImage.jpg"
-        self.classifier = PredictionPipeline(self.filename)
-
 
 @app.route("/", methods=['GET'])
 @cross_origin()
@@ -42,7 +33,5 @@ def predictRoute():
 
 
 if __name__ == "__main__":
-    clApp = ClientApp()
-    # app.run(host='0.0.0.0', port=8080) #local host
+    app.run(host='0.0.0.0', port=8080) #local host
     # app.run(host='0.0.0.0', port=8080) #for AWS
-    app.run(host='0.0.0.0', port=80) #for AZURE
